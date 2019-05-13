4764164bd9524c411dee1cfa32aa708d8ff9a3fb
Leander <Leander@124s-MBP.fritz.box>
2019-05-13 19:00:43 +0200

    Initial commit

---
+++ ChessinWindow.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+ZetCode PyQt5 tutorial
+
+This example draws three rectangles in three
+#different colours.
+
+Author: Jan Bodnar
+Website: zetcode.com
+Last edited: August 2017
+"""
+
+from PyQt5.QtWidgets import QWidget, QApplication
+from PyQt5.QtGui import QPainter, QColor, QBrush
+import sys
+
+
+class Chess(QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(200, 0, sidelen, sidelen)
+        self.setWindowTitle('Colours')
+        self.show()
+
+    def paintEvent(self, e):
+        qp = QPainter()
+        qp.begin(self)
+        self.drawBoard(qp)
+        qp.end()
+
+    def drawBoard(self, qp):
+        col = QColor(0, 0, 0)
+        qp.setPen(col)
+        qp.setBrush(QColor(0, 0, 0))
+
+        for row in range(8):
+            for col in range(4):
+                if row % 2 == 0:
+                    qp.drawRect(row * (sidelen/8),
+                                (col * (sidelen/4)) + (sidelen/8),
+                                (sidelen/8), (sidelen/8))
+                else:
+                    qp.drawRect(row * (sidelen/8),
+                                (col * (sidelen/4)),
+                                (sidelen/8), (sidelen/8))
+
+
+
+
+
+
+if __name__ == '__main__':
+    sidelen = int(input("Please type a desired sidelength for your Chessboard:"))
+    app = QApplication(sys.argv)
+    ex = Chess()
+    sys.exit(app.exec_())
